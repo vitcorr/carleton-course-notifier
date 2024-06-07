9fd34ec728f2e0946cbc3a6637e6a576985c408d
victorr <kolawoledamilola06@gmail.com>
2024-06-07 14:50:10 -0400

    added emails

--- public/client.js
+++ public/client.js
@@ -1,13 +1,16 @@
+const e = require("express");
 
 
 function submitButton(){
     const term = document.getElementById('term').value
     const crn = document.getElementById('CRN').value
+    const email = document.getElementById('email').value
+
     // const btn = document.createElement("span")
     // btn.innerHTML = term + crn
     // document.body.appendChild(btn);
     console.log(term)
-    const data = {term, crn};
+    const data = {term, crn, email};
 
     fetch('http://localhost:3000/', {
         method: "POST",
