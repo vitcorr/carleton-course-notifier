f0b4680d2784bcd8b61b1b2469cee9a844812446
victorr <kolawoledamilola06@gmail.com>
2024-07-11 21:00:08 -0400

    ssl

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const pool = new Pool({
     connectionString: 'postgresql://carleton_course_notifier_render_user:MptxTAxZYn9gEPNHPb1NiQNdLrS46ljc@dpg-cq7ktclds78s73d8qbl0-a/carleton_course_notifier_render',
-    ssl: true
+    //ssl: true
 })
 
 module.exports = pool
