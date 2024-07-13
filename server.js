9fdac4281c7bb4406019832949ca5c35fc764ad4
victorr <kolawoledamilola06@gmail.com>
2024-07-12 23:07:51 -0400

    added axios pings

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 const port = process.env.PORT || 3000;
 const nodemailer = require('nodemailer')
 require("dotenv").config();
+const axios = require('axios');
+
 
 //middleware
 app.use(express.static("public"))
@@ -361,3 +363,19 @@ async function main(){
     }
 }
 
+//ping URL
+const url = `https://carleton-course-notifier.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
