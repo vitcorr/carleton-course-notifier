1101d8258b0a38561fb589ca3d62ca56ee391b9d
victorr <kolawoledamilola06@gmail.com>
2024-08-13 16:11:12 -0400

    warnings and UI/UX changes

--- public/client.js
+++ public/client.js
@@ -2,23 +2,36 @@
 document.getElementById("Loading").style.visibility = "hidden";
 
 function submitButton(){
-    document.getElementById("Loading").style.visibility = "visible";
     const term = document.getElementById('term').value
     const crn = document.getElementById('CRN').value
     const email = document.getElementById('email').value
     const name = document.getElementById('name').value
 
     if(email === ""){
-        document.getElementById("Loading").style.visibility = "hidden";
-        alert('Enter a valid e-mail')
+        document.getElementById("error_email").style.visibility = "visible";
+        //alert('Enter a valid e-mail')
         return;
+    }else{
+        document.getElementById("error_email").style.visibility = "hidden";
     }
-    else if(name === ""){
-        document.getElementById("Loading").style.visibility = "hidden";
-        alert('Please enter your name')
+
+    if(name === ""){
+        document.getElementById("error_name").style.visibility = "visible";
+        //alert('Please enter your name')
         return;
+    }else{
+        document.getElementById("error_name").style.visibility = "hidden";
+    }
+
+    if(crn === ""){
+        document.getElementById("error_crn").style.visibility = "visible";
+        //alert('Please enter a valid crn')
+        return;
+    }else{
+        document.getElementById("error_crn").style.visibility = "hidden";
     }
     
+    document.getElementById("Loading").style.visibility = "visible";
     console.log(term)
     const data = {term, crn, email, name};
 
